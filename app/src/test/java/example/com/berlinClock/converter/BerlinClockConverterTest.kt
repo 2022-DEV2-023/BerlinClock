629614aa68e61f6a4d753600f8d308ae98d5bb42
Gert Claeskens <gert.claeskens@jidoka.be>
2022-12-08 20:02:25 +0100

    refactored tests for seconds indicator

--- app/src/test/java/example/com/berlinClock/converter/BerlinClockConverterTest.kt
+++ app/src/test/java/example/com/berlinClock/converter/BerlinClockConverterTest.kt
@@ -8,17 +8,27 @@ class BerlinClockConverterTest {
 
     @Test
     fun `returns true when seconds are even`() {
-        val input = GregorianCalendar(2022,1,1,0,0,0)
-        val actual = BerlinClockConverter.convertToBerlinClock(input)
+        val actual = calculateBerlinClockFor(seconds = 0)
 
         assertEquals(true, actual)
     }
 
     @Test
     fun `returns false when seconds are odd`() {
-        val input = GregorianCalendar(2022,1,1,0,0,1)
-        val actual = BerlinClockConverter.convertToBerlinClock(input)
+        val actual = calculateBerlinClockFor(seconds = 1)
 
         assertEquals(false, actual)
     }
+
+    private fun calculateBerlinClockFor(seconds: Int): Boolean =
+        BerlinClockConverter.convertToBerlinClock(
+            GregorianCalendar(
+                2022,
+                1,
+                1,
+                0,
+                0,
+                seconds
+            )
+        )
 }
