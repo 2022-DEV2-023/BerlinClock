e8a61a5989f635c550118266342167a2d641d8f3
Gert Claeskens <gert.claeskens@jidoka.be>
2022-12-09 10:50:47 +0100

    added ui components

--- app/src/main/java/example/com/berlinClock/MainActivity.kt
+++ app/src/main/java/example/com/berlinClock/MainActivity.kt
@@ -3,13 +3,24 @@ package example.com.berlinClock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import example.com.berlinClock.ui.theme.BerlinClockTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +41,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(showBackground = true)
+fun Rectangle() {
+    Box(
+        modifier = Modifier
+            .width(50.dp)
+            .height(25.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .border(2.dp, Color.Black, RoundedCornerShape(10.dp))
+            .background(Color.Red)
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BerlinClockTheme {
-        Greeting("Android")
-    }
+@Preview(showBackground = true)
+fun Circle() {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .clip(CircleShape)
+            .border(2.dp, Color.Black, CircleShape)
+            .background(Color.Red)
+    )
 }
