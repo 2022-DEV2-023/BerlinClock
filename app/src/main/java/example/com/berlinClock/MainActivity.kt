151947731b21e4fe49dd4cc0ddb5e7e791937d2c
Gert Claeskens <gert.claeskens@jidoka.be>
2022-12-09 10:57:08 +0100

    building empty state

--- app/src/main/java/example/com/berlinClock/MainActivity.kt
+++ app/src/main/java/example/com/berlinClock/MainActivity.kt
@@ -5,20 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import example.com.berlinClock.ui.theme.BerlinClockTheme
@@ -26,6 +32,10 @@ import example.com.berlinClock.ui.theme.BerlinClockTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val defaultModifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+
         setContent {
             BerlinClockTheme {
                 // A surface container using the 'background' color from the theme
@@ -33,7 +43,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = defaultModifier,
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = defaultModifier,
+                        ) {
+                            Circle()
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = defaultModifier,
+                        ) {
+                            for (i in 1..4) {
+                                Rectangle()
+                            }
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = defaultModifier,
+                        ) {
+                            for (i in 1..4) {
+                                Rectangle()
+                            }
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = defaultModifier,
+                        ) {
+                            for (i in 1..11) {
+                                Rectangle()
+                            }
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = defaultModifier,
+                        ) {
+                            for (i in 1..4) {
+                                Rectangle()
+                            }
+                        }
+                    }
                 }
             }
         }
