ad060629484c7ef71e8686cb7e1e24d9b549004b
Gert Claeskens <gert.claeskens@jidoka.be>
2022-12-09 11:35:39 +0100

    added viewmodel method to start clock

--- app/src/main/java/example/com/berlinClock/viewmodel/BerlinClockViewModel.kt
+++ app/src/main/java/example/com/berlinClock/viewmodel/BerlinClockViewModel.kt
@@ -1,11 +1,14 @@
 package example.com.berlinClock.viewmodel
 
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.ViewModel
 import example.com.berlinClock.converter.BerlinClockConverter
 import example.com.berlinClock.models.BerlinClockValues
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import java.util.*
 
 
@@ -14,4 +17,16 @@ class BerlinClockViewModel(date: GregorianCalendar = GregorianCalendar()) : View
         MutableStateFlow(BerlinClockConverter.convertToBerlinClock(date))
     val uiState: StateFlow<BerlinClockValues> = _uiState.asStateFlow()
 
+    fun startClock() {
+        Handler(Looper.getMainLooper()).also {
+            it.post(object : Runnable {
+                override fun run() {
+                    _uiState.update {
+                        BerlinClockConverter.convertToBerlinClock()
+                    }
+                    it.postDelayed(this, 1000)
+                }
+            })
+        }
+    }
 }
